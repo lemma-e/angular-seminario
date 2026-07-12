fd9224862513ca372a6cd96a8482d51ea63a92b1
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-07-12 18:56:56 -0300

    add routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Classes } from './pages/classes/classes';
+import { Dungeons } from './pages/dungeons/dungeons';
+import { Recommender } from './pages/recommender/recommender';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'', component: Home},
+  {path: 'classes', component: Classes},
+  {path: 'dungeons', component: Dungeons},
+  {path: 'recommender', component: Recommender}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
